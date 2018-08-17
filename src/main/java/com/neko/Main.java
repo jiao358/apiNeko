caf3e529924ea0d45cc4c1552216acedb6fb6999
fuming.lj <fuming.lj@alibaba-inc.com>
2018-08-17 19:29:12 +0800

    update

--- src/main/java/com/neko/Main.java
+++ src/main/java/com/neko/Main.java
@@ -8,8 +8,10 @@ import com.alibaba.fastjson.JSONObject;
 public class Main {
     public static void main(String[] args) throws Exception{
         ApiClient apiClient= new ApiClient();
+        long begin = System.currentTimeMillis();
         String ab= apiClient.getOrderInfo("9339040475");
         System.out.println("获取 getOrderInfo 接口"+ab);
+        System.out.println("查询时间:"+(System.currentTimeMillis()-begin)+"ms");
 
         String account=apiClient.getAccountId();
         System.out.println("获取 account 接口:"+ account);
